d3870e8542010279650d117a8169d06a3e0469df
Gavin M. Roy <gavinmroy@gmail.com>
2013-10-04 09:37:31 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'gads'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'MeetMe, Inc.'
+maintainer_email 'gmr@meetme.com'
 license          'All rights reserved'
 description      'Installs/Configures gads'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends          'java'
